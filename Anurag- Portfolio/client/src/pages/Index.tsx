dcc9f8a0ba90bc9ffa0a19ac4a201027a39f55dc
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-08-02 19:06:22 +0530

    Update Index.tsx

--- Anurag- Portfolio/client/src/pages/Index.tsx
+++ Anurag- Portfolio/client/src/pages/Index.tsx
@@ -8,8 +8,6 @@ import Navigation from '@/components/Navigation';
 const Index = ({access}) => {
 
   return (
-    // <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
-    // <div className="min-h-screen bg-background ">
 
     <div className="min-h-screen bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900">
     
