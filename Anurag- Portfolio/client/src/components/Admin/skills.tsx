732d55a1a1ffd086e6e42d557072f1d3b25cd063
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-08-02 19:22:32 +0530

    Update skills.tsx

--- Anurag- Portfolio/client/src/components/Admin/skills.tsx
+++ Anurag- Portfolio/client/src/components/Admin/skills.tsx
@@ -12,7 +12,7 @@ import party  from 'party-js';
 
 const Skills = () => {
 
-   const baseURL = import.meta.env.VITE_REACT_APP_API_BASE_URL;
+   const baseUrl = import.meta.env.VITE_REACT_APP_API_BASE_URL;
     
   const [suggestedTechsAi, setSuggestedTechsAi] = useState([]);
   const [isEdit,setisEdit] = useState(false)
