567f781818f2395de5e82bc1b9e7fba320d5eb76
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-09-18 23:21:08 +0530

    Update NotFound.tsx

--- Hostel Manager/client/src/pages/NotFound.tsx
+++ Hostel Manager/client/src/pages/NotFound.tsx
@@ -17,11 +17,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-<<<<<<<< HEAD:Quiz Master/client/src/pages/NotFound.tsx
-        <a href="/dashboard" className="text-blue-500 hover:text-blue-700 underline">
-========
         <Link to="/" className="text-blue-500 hover:text-blue-700 underline" replace>
->>>>>>>> 3b028b90 (New Members):Hostel Manager/client/src/pages/NotFound.tsx
           Return to Home
         </Link>
       </div>
