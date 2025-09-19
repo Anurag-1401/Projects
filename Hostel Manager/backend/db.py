db606376dc2f958d283a5715a2a5ad1ee354ea3d
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-09-19 21:49:27 +0530

    Update db.py

--- Hostel Manager/backend/db.py
+++ Hostel Manager/backend/db.py
@@ -25,19 +25,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import sqlitecloud
+# import sqlitecloud
 
-# Your SQLite Cloud connection string
-SQLITE_CLOUD_URL = "sqlitecloud://cm7belajhk.g5.sqlite.cloud:8860/auth.sqlitecloud?apikey=dma7H6ywCzn7U5bjHwaVbMibC1cwH11Pw8KWOf1kNCk"
+# # Your SQLite Cloud connection string
+# SQLITE_CLOUD_URL = "sqlitecloud://cm7belajhk.g5.sqlite.cloud:8860/auth.sqlitecloud?apikey=dma7H6ywCzn7U5bjHwaVbMibC1cwH11Pw8KWOf1kNCk"
 
-def get_sqlitecloud_connection():
-    return sqlitecloud.connect(SQLITE_CLOUD_URL)
+# def get_sqlitecloud_connection():
+#     return sqlitecloud.connect(SQLITE_CLOUD_URL)
 
-# Use creator so SQLAlchemy uses sqlitecloud connection
-engine = create_engine(
-    "sqlite://",  # dummy, but required by SQLAlchemy
-    creator=get_sqlitecloud_connection
-)
+# # Use creator so SQLAlchemy uses sqlitecloud connection
+# engine = create_engine(
+#     "sqlite://",  # dummy, but required by SQLAlchemy
+#     creator=get_sqlitecloud_connection
+# )
+
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True) 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
