d09d1a939d82e06c46f15b8c11a1261143b2f38e
Anurag-1401 <bhavthankaranurag@gmail.com>
2025-09-21 18:41:12 +0530

    merged

--- Hostel Manager/backend/run.py
+++ Hostel Manager/backend/run.py
@@ -2,3 +2,13 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=10000)
+
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         app="main:app",  # "main" is your main.py, "app" is the FastAPI instance
+#         host="127.0.0.1",  # localhost only
+#         port=8000,         # port number
+#         reload=True        # auto-reload on code changes (development only)
+#     )
