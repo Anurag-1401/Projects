129a739d3090274c6fca6dd4a9a2f9baca6db3ee
Anurag Dnyaneshwar Bhavthankar <bhavthankaranurag@gmail.com>
2025-11-04 22:38:06 +0530

    Set up database tables and modify CORS origins
    Initialize database tables and update CORS settings.

--- Hostel Manager/backend/main.py
+++ Hostel Manager/backend/main.py
@@ -1,30 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from db import engine
+from db import engine, Base
 import allModels
 from CRUD import attendanceJob
 from ROUTER import routes
 
 app = FastAPI()
 
-# CORS
+allModels.Base.metadata.create_all(bind=engine)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sggshostel.vercel.app"],
+    allow_origins=[
+        "http://localhost:8080",
+        "https://sggshostel.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Create tables if not exists
-allModels.Base.metadata.create_all(bind=engine)
-
-# Include routers
 app.include_router(routes.router)
 
-# Start scheduler on startup
 @app.on_event("startup")
-def startup_event():
+async def startup_event():
     attendanceJob.start_scheduler()
 
 @app.get('/')
